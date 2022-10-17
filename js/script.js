1833b319af5d41996fee98b7d7445a3dda4c7d06
Oleg <olegpidgaetskiy@gmail.com>
2022-10-17 21:22:04 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let answers = [];
+
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What is your surname?", "");
+answers[2] = prompt("How years old?", "");
+document.write(answers);
