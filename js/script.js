f2ec1b8a604a590cf7cc25405156d32264c5a534
Oleg <olegpidgaetskiy@gmail.com>
2022-10-20 21:35:49 +0300

    .udemy

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-"use strict";
 
-let answers = [];
 
-answers[0] = prompt("What is your name?", "");
-answers[1] = prompt("What is your surname?", "");
-answers[2] = prompt("How years old?", "");
-document.write(answers);
+
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your surname?", "");
+// answers[2] = prompt("How years old?", "");
+// let answers = [];
+
+let numberOfLilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+let personalMovieDB = {
+        count: numberOfLilms,
+        movies: {},
+        actors:{},
+        genres: [],
+        privat: false
+    };
+
+    let a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", ""),
+          c = prompt("Один из последних просмотренных фильмов?", ""),
+          d = prompt("На сколько оцените его?", "");
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
+
+// if(4 == 9) {
+//     console.log("Ok");
+// } else {
+//     console.log("Error");
+// };
+
+// const num = 50;
+
+// if (num<49){
+
+// }
