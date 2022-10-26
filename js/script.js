db20d0bf39e71e1d7348fb07435ad028d29e9b7b
Oleg <olegpidgaetskiy@gmail.com>
2022-10-26 21:45:44 +0300

    Added HTML header

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@
 // answers[2] = prompt("How years old?", "");
 // let answers = [];
 
-let numberOfLilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+// let numberOfLilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-let personalMovieDB = {
-        count: numberOfLilms,
-        movies: {},
-        actors:{},
-        genres: [],
-        privat: false
-    };
+// let personalMovieDB = {
+//         count: numberOfLilms,
+//         movies: {},
+//         actors:{},
+//         genres: [],
+//         privat: false
+//     };
 
-    let a = prompt("Один из последних просмотренных фильмов?", ""),
-          b = prompt("На сколько оцените его?", ""),
-          c = prompt("Один из последних просмотренных фильмов?", ""),
-          d = prompt("На сколько оцените его?", "");
+//     let a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?", ""),
+//           c = prompt("Один из последних просмотренных фильмов?", ""),
+//           d = prompt("На сколько оцените его?", "");
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
 
-    console.log(personalMovieDB);
+//     console.log(personalMovieDB);
 
 // if(4 == 9) {
 //     console.log("Ok");
@@ -38,3 +38,36 @@ let personalMovieDB = {
 // if (num<49){
 
 // }
+// const btn = document.querySelector('button');
+
+// btn.onclick = function(){
+//     alert('Click');
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+// btn.addEventListener('click', () => {
+//     alert('Second click');
+// });
+    // btn.addEventListener('click', (event) => {
+    //     console.log(event.target);
+    //     event.target.remove();
+    // });
+    const btn = document.querySelector('button');
+    overlay = document.querySelector('.overlay');
+
+    let i = 0;
+    const deleteElement = (e) => {
+        console.log(e.currentTarget);
+        console.log(e.type);
+        // i++;
+        // if(i == 1){
+        //     btn.addEventListener('click', deleteElement);
+        // }
+    };
+
+    btn.addEventListener('click', deleteElement);
+    overlay.addEventListener('click', deleteElement);
+    
+
